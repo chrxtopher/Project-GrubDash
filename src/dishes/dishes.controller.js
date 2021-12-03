36856ebd4eb1f8021108ff2926357ab6aaaa363a
Christopher Roddy <chrxtopherxddy@gmail.com>
2021-12-03 15:33:29 -0700

    Route for "/dishes/:dishId" is up and working!
    Also added an additional check to the bodyPropertiesAreEmpty function to check if the price property is an integer.

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -22,6 +22,14 @@ function create(req, res, next) {
 }
 
 //////////
+// READ //
+//////////
+
+function read(req, res, next) {
+  res.json({ data: res.locals.dish });
+}
+
+//////////
 // LIST //
 //////////
 
@@ -33,10 +41,26 @@ function list(req, res) {
 // VALIDATION MIDDLEWARE BELOW //
 /////////////////////////////////
 
+function dishExists(req, res, next) {
+  // this function will check the list of dishes for a particular dish.
+  // if it does not, will return an error status and message.
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    return next();
+  }
+
+  next({
+    status: 404,
+    message: `That dish does not exist! Dish id not found: ${dishId}`,
+  });
+}
+
 function allBodyPropertiesExist(req, res, next) {
   // this function will check if all body props exist for post & put methods.
   // if one or more do not, it will return an error status and message.
-
   const { data: { name, description, price, image_url } = {} } = req.body;
 
   if (name && description && price && image_url) {
@@ -54,19 +78,19 @@ function bodyPropertiesAreEmpty(req, res, next) {
   // this function will check if name, description, or image_url are empty.
   // will also check if price is zero or less.
   // if any above are true, will return error status and message.
-
   const { data: { name, description, price, image_url } = {} } = req.body;
 
   if (
     name.length === 0 ||
     description.length === 0 ||
     price <= 0 ||
+    !Number.isInteger(price) ||
     image_url.length === 0
   ) {
     return next({
       status: 400,
       message:
-        "Name, description, and image_url properties cannot be empty. Price property cannot be zero or less.",
+        "Name, description, and image_url properties cannot be empty. Price property must be an integer that is not zero or less.",
     });
   }
 
@@ -75,5 +99,6 @@ function bodyPropertiesAreEmpty(req, res, next) {
 
 module.exports = {
   create: [allBodyPropertiesExist, bodyPropertiesAreEmpty, create],
+  read: [dishExists, read],
   list,
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const controller = require("./dishes.controller");
 
+router.route("/:dishId").get(controller.read);
 router.route("/").get(controller.list).post(controller.create);
 
 module.exports = router;
